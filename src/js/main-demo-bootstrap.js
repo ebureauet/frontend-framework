f6f1b26bf2635a0c4a9c8ae95bd1d498cd046942
teej043 <teej.ebureauet@gmail.com>
2015-04-21 22:48:10 +0800

    added bootstrap demos

--- src/js/main-demo-bootstrap.js
+++ src/js/main-demo-bootstrap.js
@@ -1,4 +1,4 @@
 //These scripts will be bundled
 require('../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
 require('./general/carousel.js');
-require('./general/custom.js');
+//require('./general/custom.js');
